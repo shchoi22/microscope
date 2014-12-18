0f6db59b44885bd78e9427e06414aee7f560568c
Stan Choi <shchoi22@gmail.com>
2014-12-18 06:55:50 -0600

    initial commit

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,22 @@
+Template.postSubmit.events({
+	'submit form': function(events) {
+		events.preventDefault();
+
+		var post = {
+			url: $(events.target).find('[name=url]').val(),
+			title: $(events.target).find('[name=title]').val()
+		};
+
+		Meteor.call('postInsert', post, function(error,result) {
+			if (error) {
+				return alert(error.reason);
+			}
+
+			if (result.postExists) {
+				alert('This Link has already been posted');
+			}
+
+			Router.go('postPage', {_id: result._id});
+		});
+	}
+})
